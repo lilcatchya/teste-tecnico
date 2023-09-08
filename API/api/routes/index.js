4085b06e47bb332044e900d78f3f29288abeabf1
Ricardo Franco <ricardofranco2509@gmail.com>
2023-09-08 15:33:13 -0300

    implementação do cors

--- API/api/routes/index.js
+++ API/api/routes/index.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser')
 const router = require('./router')
+const cors = require('cors');
 
 module.exports = app => {
   app.use(bodyParser.json())
   app.use(router)
+  app.use(cors({origin: '*'}))
 }
